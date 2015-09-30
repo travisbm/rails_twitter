f5eb86348ce5f066b5a3abcb2112172161e030fb
Travis Montgomery <travisbm@gmail.com>
2015-09-30 15:09:28 -0500

    updated seeds.rb. added materialize @import to application.scss

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,7 @@ tweet_count = [1, 3, 5, 7]
     first_name: Faker::Name.first_name
     last_name:  Faker::Name.last_name
     username:   Faker::Internet.user_name
+    password_digest: 'password'
     })
 
   tweet_count.sample times do
