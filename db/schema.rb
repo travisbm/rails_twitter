76b42a4a23b11041ab75985269e23ab8335d36a2
Travis Montgomery <travisbm@gmail.com>
2015-09-30 21:11:58 -0500

    added email column to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150930194241) do
+ActiveRecord::Schema.define(version: 20151001021041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20150930194241) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "email"
   end
 
   add_foreign_key "tweets", "users"
