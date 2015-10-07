1ee386bff7c51554d273faa909155fb202511c24
Travis Montgomery <travisbm@gmail.com>
2015-10-07 12:00:48 -0500

    syntax cleanup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,15 +24,4 @@ $( document ).ready(function() {
   });
 
   // var userTweets = $( '.user_tweets' );
-
-  // $(".tweet_button").on('click', function(){
-  //   $(".user_tweets").css('background-color', 'blue')
-  // });
-
-  //   $(".tweet_button").toggle( function(){
-  //     $(".user_tweets").css('background-color', 'blue');
-  // },function(){
-  //     $(".user_tweets").css('background-color', 'pink');
-  //   });
-  // }
 });
